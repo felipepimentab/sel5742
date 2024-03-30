a84cc7194d9412e905b6a17d1076bd5e0edfbc61
Felipe Pimenta <felipe.pbernardo@gmail.com>
2024-03-30 17:20:12 -0300

    changes matrix name on file module

--- utils/file.py
+++ utils/file.py
@@ -6,12 +6,12 @@ def text_to_table(path: str) -> tuple[np.ndarray, int, int]:
   """
   rows = open(path, 'r').readlines() # reads txt data as an array of its lines
   k = len(rows) # sets the number of rows (samples)
-  n = len(rows[0].split()) # sets the number of columns (inputs)
-  x = np.zeros((k, n)) # creates a placeholder matrix for "x"
+  n = len(rows[0].split()) # sets the number of columns
+  data = np.zeros((k, n)) # creates a placeholder matrix for "data"
 
   for i in range(k):
     line = rows[i].split() # creates an array by spliting the line on every whitespace/tab
     for j in range(n):
-      x[i][j] = float(line[j]) # assings the corresponding value to the table as a float
+      data[i][j] = float(line[j]) # assings the corresponding value to the table as a float
   
-  return x, k, n
+  return data, k, n
